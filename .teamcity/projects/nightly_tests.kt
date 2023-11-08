b54362fa84099c3fc61b5e8598a8ac544a34d0c7
Sarah French <sarah.french@hashicorp.com>
2023-11-08 15:31:48 +0000

    Try get chart to show

--- .teamcity/projects/nightly_tests.kt
+++ .teamcity/projects/nightly_tests.kt
@@ -2,6 +2,9 @@ package projects
 import jetbrains.buildServer.configs.kotlin.*
 import builds.AccTestBuildConfig
 import jetbrains.buildServer.configs.kotlin.ui.createProjectFeature
+import jetbrains.buildServer.configs.kotlin.CustomChart.Serie
+import jetbrains.buildServer.configs.kotlin.CustomChart.SeriesKey
+
 
 val NightlyTestsProjectId = RelativeId("NightlyTests")
 
@@ -19,7 +22,10 @@ fun NightlyTests() : Project {
                 id = "ProjectCustomChart_1"
                 title = "Chart Title"
                 seriesTitle = "Series Title"
-                format = CustomChart.Format.DURATION
+                format = CustomChart.Format.PERCENT
+                series = listOf(
+                    Serie(title = "Success Rate", key = SeriesKey.SUCCESS_RATE, sourceBuildTypeId = AbsoluteId("${NightlyTestsProjectId}_my_build_${1}"))
+                )
             }
         }
 
